340b6aeb90f1640954dabca4fc48d4285407bcfd
A.Kotinov <alexander777ck@gmail.com>
2022-08-23 13:56:22 +0300

    Added plasmic data.

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,30 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
+    {
+      resolve: '@plasmicapp/loader-gatsby',
+      options: {
+        projects: [
+          {
+            id: 'dzpgr8FiKTRG1eV2baU5aV', // ID of a project you are using
+            token: 'FB75RvsMyu5NGVvj162SybtopbIKiF1ZN47phpBHE8kY5BIthLucpeI9nfef5bd1nBIhZQ7duLB2KOrjF3Rg' // API token for that project
+          }
+        ],
+        // Fetches the latest revisions, whether or not they were unpublished!
+        // Disable for production to ensure you render only published changes.
+        preview: true,
+        defaultPlasmicPage: require.resolve('./src/templates/defaultPlasmicPage.tsx'),
+        // Optionally specify pages to ignore.
+        ignorePaths: ['/my-page']
+      }
+    }
   ],
 }
